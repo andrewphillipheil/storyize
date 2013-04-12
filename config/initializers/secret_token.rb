bc97cde846f622b803f249f9d142d67ad8174f84
Andrew <aph7824@gmail.com>
2013-04-12 18:13:39 -0400

    empty project commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Storyize::Application.config.secret_token = 'd3061817f3d617fdb22a23da2c30e05f48ce61e589fff340271f68c30669a693e2d44a18da01ed015350d810f6037787ec7520da4e268a9c887195565423a43a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Storyize::Application.config.session_store :cookie_store, key: '_storyize_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Storyize::Application.config.session_store :active_record_store
